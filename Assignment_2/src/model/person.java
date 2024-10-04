a61455cd6ec1c7a1d3b86b8097b9dc8531e309e6
Sanskruti Mahajan <sanskruti@Sanskrutis-MacBook-Air.local>
2024-10-04 17:00:31 -0400

    Add assignment2 project

---
+++ Assignment_2/src/model/person.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author sanskruti
+ */
+public class person {
+    private String firstName;
+    private String lastName;
+    private long ssn;  // Social Security Number
+    private int age;
+    private String homeAddress;
+    private String workAddress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(long ssn) {
+        this.ssn = ssn;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(String homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public String getWorkAddress() {
+        return workAddress;
+    }
+
+    public void setWorkAddress(String workAddress) {
+        this.workAddress = workAddress;
+    }
+  
+}
---
+++ Assignment_2/src/model/personDirectory.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+import java.util.ArrayList;
+/**
+ *
+ * @author sanskruti
+ */
+public class personDirectory {
+      private ArrayList<Person> personList;
+}
